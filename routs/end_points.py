3271e930d8f7ef836606c1cbb1eebb0ccc1e90e8
Felipe Dias <desenvolvimento4@izi.agr.br>
2023-07-07 16:57:54 -0300

    teste

--- routs/end_points.py
+++ routs/end_points.py
@@ -1,8 +1,21 @@
-
-from Database.user_sql import get_carros as car
+from Database.user_sql import get_table_carros as car
+from Database.user_sql import save_user as user
 from flask import Blueprint, jsonify, request
 
 user_routes = Blueprint("user", __name__, url_prefix="/user")
+
+
+@user_routes.route("/get_usuario/", methods=['POST'])
+def get_user():
+    name = request.args.get("name", None, str)
+    email = request.args.get("email", None, str)
+    senha = request.args.get("senha", None, str)
+
+    usuario = user(name, email, senha)
+
+    response = jsonify(usuario)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response, 200
 
 
 @user_routes.route("/get_carros/", methods=['GET'])
