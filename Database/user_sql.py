883ea00fbcb38dfc45b61355352e668043a3e07f
Felipe Dias <desenvolvimento4@izi.agr.br>
2023-07-07 16:57:12 -0300

    teste

--- Database/user_sql.py
+++ Database/user_sql.py
@@ -1,4 +1,3 @@
-
 import psycopg2
 
 from psycopg2.extras import DictCursor
@@ -6,7 +5,7 @@ from psycopg2.extras import DictCursor
 
 def get_connect():
     return psycopg2.connect(
-        host="192.168.0.7",
+        host="localhost",
         port="5432",
         database="postgres",
         user="postgres",
@@ -14,7 +13,17 @@ def get_connect():
     )
 
 
-def get_carros():
+def save_user(name=None, email=None, senha=None):
+    with get_connect() as conn:
+        cur = conn.cursor()
+
+        cur.execute("INSERT INTO cadastro (nome, email, senha) VALUES (%s, %s, %s);", (name, email, senha))
+        conn.commit()
+
+        return True
+
+
+def get_table_carros():
     with get_connect() as conn:
         cur = conn.cursor(cursor_factory=DictCursor)
 
