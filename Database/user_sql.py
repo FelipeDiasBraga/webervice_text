f177185c9f995d05b03417c5f86a5677388df4cc
Felipe Dias <desenvolvimento4@izi.agr.br>
2023-08-01 18:20:43 -0300

    definiçoes que estruturam os dados em json

--- Database/user_sql.py
+++ Database/user_sql.py
@@ -1,3 +1,4 @@
+
 import psycopg2
 
 from psycopg2.extras import DictCursor
@@ -14,32 +15,84 @@ def get_connect():
     )
 
 
-def save_user(name=None, email=None, senha=None):
-
+def save_user(nome, email, telefone, endereco):
     with get_connect() as conn:
-        cur = conn.cursor()
-        select = f"INSERT INTO public.cadastro (nome, email, senha) VALUES ('{name}', '{email}', '{senha}');"
+
+        cur = conn.cursor(cursor_factory=DictCursor)
+        select = f"insert into postgres.compras.clientes (nome, email, telefone, endereco) " \
+                 f"values ('{nome}', '{email}', '{telefone}', '{endereco}')"
         cur.execute(select)
         conn.commit()
 
-        return True
+    return True
 
 
-def get_table_carros():
+def get_list_clientes():
     with get_connect() as conn:
         cur = conn.cursor(cursor_factory=DictCursor)
-
-        cur.execute("SELECT * FROM carros")
-        data = cur.fetchall()
-
-    car = [x for x in data]
-
-    return car
-
-
-def get_dados_vadildate():
-    with get_connect() as conn:
-        cur = conn.cursor(cursor_factory=DictCursor)
-        cur.execute(f"SELECT nome from public.cadastro;")
+        cur.execute(f"select c.nome from postgres.compras.clientes c")
         dados = cur.fetchall()
-        print(dados)
+        data = []
+        [data.append(i.get("nome")) for i in dados]
+
+    return rows_dict_clientes(data)
+
+
+def get_estoque(produtos=False, id=None):
+    with get_connect() as conn:
+        cur = conn.cursor(cursor_factory=DictCursor)
+        if produtos:
+            cur.execute(f"SELECT p.id_produto, p.nome_produto, p.quantidade_estoque "
+                        f"FROM compras.produtos p where id_produto ={id}")
+            dados = cur.fetchall()
+            dict_dados = [dict({
+                "dados": rows_dict_produtoas([i for i in dados])
+            })]
+            print([i for i in dados])
+    return dict_dados
+
+
+def dados_json_bd():
+    with get_connect() as conn:
+        cur = conn.cursor(cursor_factory=DictCursor)
+        select = f"select * from postgres.compras.dados_json_items "
+        cur.execute(select)
+        nota = cur.fetchall()
+        print(nota)
+        dados_js = [i for i in nota]
+        lista2 = []
+        for i in dados_js:
+            lista2.append(i)
+        for i in range(len(lista2)):
+            # rows = rows_dados_da_nota(lista2[i][0],lista2[i][0][1], lista2[i][0][2], lista2[i][0][3],lista2[i][0][4])
+
+            rows1 = rows_dados_da_nota(lista2[i][1][0],lista2[i][1][1], lista2[i][1][2], lista2[i][1][3],lista2[i][1][4])
+
+    return dict({
+        # "1": rows,
+        "2": rows1
+    })
+
+
+def rows_dados_da_nota(id, produto, qtd_estoque, qtd, nome):
+    return dict(
+        {
+            "id": f'{id}',
+            "nome_produto": f"{produto}",
+            "quantidade_em_estoque": qtd_estoque,
+            "quantidade": qtd,
+            "nome_usuario":f"{nome}"
+        }
+    )
+
+
+def rows_dict_produtoas(row):
+    return dict({
+        "nome_produtos": row
+    })
+
+
+def rows_dict_clientes(row):
+    return dict({
+        "lista_de_nomes": row
+    })
--- routs/end_points.py
+++ routs/end_points.py
@@ -1,30 +1,82 @@
-from Database.user_sql import get_table_carros as car
-from Database.user_sql import get_dados_vadildate as valid
+import json
+
+from Database.user_sql import get_list_clientes as clientes
+from Database.user_sql import get_estoque as estoque
 from Database.user_sql import save_user as user
+from Database.user_sql import dados_json_bd as dados_json
 from flask import Blueprint, jsonify, request
 
-user_routes = Blueprint("user", __name__, url_prefix="/user")
+user_routes = Blueprint("/user", __name__, url_prefix="/user")
 
 
-@user_routes.route("/get_usuario/", methods=['POST'])
-def get_user():
+@user_routes.route("/get_user_json/", methods=['POST', 'GET'])
+def get_user_json():
+    # um endpoint que recebe os metodo de argumentos como json
+    # usado para quando preciso resceber dados em listas ou json
     args = request.get_json()
-    name = args.get("name", None)
-    email = args.get("email", None)
-    senha = args.get("senha", None)
 
-    usuario = user(name, email, senha)
+    nome = args.get("nome", str)
+    email = args.get("email", str)
+    telefone = args.get("telefone", str)
+    endereco = args.get("endereco", str)
+
+    usuario = user(nome, email, telefone, endereco)
+
+    print(f"nome:{nome}", f"email:{email}", f"telefone:{telefone}", f"endereço:{endereco}")
+
+    response = jsonify(usuario)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response, 200
+
+
+@user_routes.route("/get_user/", methods=['GET'])
+def get_user():
+    # um endpoint que recebe os dados com metodo de argumentos normal do tipo args
+
+    nome = request.args.get("nome", str)
+    email = request.args.get("email", str)
+    telefone = request.args.get("telefone", str)
+    endereco = request.args.get("endereco", str)
+
+    usuario = user(nome, email, telefone, endereco)
+
+    print(f"nome:{nome}", f"email:{email}", f"telefone:{telefone}", f"endereço:{endereco}")
 
     response = jsonify(usuario)
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response, 200
 
 
-@user_routes.route("/get_carros/", methods=['GET'])
-def get_carros():
-    carros = car()
-
-    response = jsonify(carros)
+@user_routes.route("/get_clientes/", methods=['GET'])
+def get_clientes():
+    rows_clientes = clientes()
+    response = jsonify(rows_clientes)
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response, 200
+
+
+@user_routes.route("/get_estoque/", methods=["GET"])
+def get_estoque():
+    produtos = request.args.get("produtos", False, int)
+    id = request.args.get("id", None, int)
+    if produtos and id:
+        poduct = estoque(produtos, id)
+
+        response = jsonify(poduct)
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response, 200
+    else:
+        select = "dados inesitentes"
+        response = jsonify(select)
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response, 200
+
+
+@user_routes.route("/get_dados_json_direto_do_banco/", methods=["GET"])
+def get_tranformat_em_json():
+    response = jsonify(dados_json())
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response, 200
+
 
